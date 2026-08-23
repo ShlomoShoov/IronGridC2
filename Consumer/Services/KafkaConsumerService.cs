18415383336fc693161a475d79a8f14343b617da
ShlomoShoov <shloshoov@gmail.com>
2026-08-23 16:34:55 +0300

    feature:Adding Processing

--- Consumer/Services/KafkaConsumerService.cs
+++ Consumer/Services/KafkaConsumerService.cs
@@ -1,12 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Threading.Tasks;
+using Confluent.Kafka;
+using Consumer.Models;
 
 namespace Consumer.Services
 {
-    public class KafkaConsumerService
+    public class KafkaConsumerService : IDisposable
     {
-        
+        private IConsumer<Null, string> _consumer;
+        private IIRonGridLogger _logger;
+        private string _serviceName = "Kafka Consumer Service";
+
+        public KafkaConsumerService(KafkaConsumerSetting settings, IIRonGridLogger logger)
+        {
+            _logger = logger;
+            ConsumerConfig consumerConfigs = new ConsumerConfig()
+            {
+                BootstrapServers = settings.BootstrapServers,
+                GroupId = settings.GroupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            _consumer = new ConsumerBuilder<Null, string>(consumerConfigs).Build();
+            _consumer.Subscribe([settings.PerimeterSensorTopicName, settings.UAVTopicName]);
+        }
+
+        public ConsumeResult<Null, string> Consume()
+        {
+            return _consumer.Consume();
+        }
+
+        public void Dispose()
+        {
+            _consumer.Unsubscribe();
+            _consumer.Dispose();
+        }
     }
 }
--- Consumer/Services/ReportsProcessorService.cs
+++ Consumer/Services/ReportsProcessorService.cs
@@ -1,12 +1,97 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Confluent.Kafka;
+using Consumer.Models;
 
 namespace Consumer.Services
 {
     public class ReportsProcessorService
     {
-        
+        private KafkaConsumerSetting _settings;
+
+        public ReportsProcessorService(KafkaConsumerSetting setting)
+        {
+            _settings = setting;
+        }
+        public AssetLiveStatus ProcessReport(ConsumeResult<Null, string> ReportEvent)
+        {
+            Report report = JsonSerializer.Deserialize<Report>(ReportEvent.Message.Value)!;
+            
+            AssetLiveStatus liveStatus = new AssetLiveStatus
+            {
+                AssetId = report.AssetId,
+                AssetType = report.AssetType,
+                Timestamp = report.Timestamp,
+            };
+            
+
+            if (ReportEvent.Topic == _settings.PerimeterSensorTopicName)
+            {
+                _CalculatePerimeterSensor(report, liveStatus);
+            }
+
+            else if (ReportEvent.Topic == _settings.UAVTopicName)
+            {
+                _CalculateUAV(report, liveStatus);
+            }
+
+            return liveStatus;
+
+
+        }
+
+        private void _CalculatePerimeterSensor(Report report, AssetLiveStatus liveStatus)
+        {
+            string rawValue = report.RawValue.Trim();
+            liveStatus.RawValue = rawValue;
+            if (!int.TryParse(rawValue, out int rawAsInt) || rawAsInt< 0 || rawAsInt > 100)
+            {
+                liveStatus.ProcessedStatus = ProcessedStatus.Warning;
+                liveStatus.IsVerified = false;
+            }
+            else if (rawAsInt < 20)
+            {
+                liveStatus.ProcessedStatus = ProcessedStatus.Warning;
+                liveStatus.IsVerified = true;
+            }
+            else
+            {
+                liveStatus.ProcessedStatus = ProcessedStatus.Stable;
+                liveStatus.IsVerified = true;
+            }
+        }
+        private void _CalculateUAV(Report report, AssetLiveStatus liveStatus)
+        {
+            List<string> StableStatuses = ["Good", "GOOD", "good", "gud"];
+            
+            List<string> WarningStatuses = ["Bad", "BAD", "bad", "bed"];
+            string rawValue = report.RawValue.Trim();
+            if (StableStatuses.Contains(rawValue))
+            {
+                liveStatus.RawValue = "Good";
+                liveStatus.ProcessedStatus = ProcessedStatus.Stable;
+                liveStatus.IsVerified = true;
+            }
+            else if (WarningStatuses.Contains(rawValue))
+            {
+                liveStatus.RawValue = "Bad";
+                liveStatus.ProcessedStatus = ProcessedStatus.Warning;
+                liveStatus.IsVerified = true;
+            }
+            else
+            {
+                liveStatus.ProcessedStatus = ProcessedStatus.Warning;
+                liveStatus.IsVerified = false;
+                liveStatus.RawValue = rawValue;
+            }
+            
+        }
+
+
+
+
     }
 }
