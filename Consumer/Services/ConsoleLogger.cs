1382202fef942395a39c517f23fc7cb4429fe5ec
ShlomoShoov <shloshoov@gmail.com>
2026-08-24 17:18:28 +0300

    feture: changing the logger

--- Consumer/Services/ConsoleLogger.cs
+++ Consumer/Services/ConsoleLogger.cs
@@ -9,17 +9,33 @@ namespace Consumer.Services
     {
         public void Debug(string serviceName, string eventDetails)
         {
-            System.Console.WriteLine($"Debug | {serviceName} | {eventDetails}");
+            _DisplayServiceName(serviceName);
+            Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine($"| Debug | {eventDetails}");
+            Console.ResetColor();
+
         }
         public void Warning(string serviceName, string eventDetails)
         {
-            System.Console.WriteLine($"Warning | {serviceName} | {eventDetails}");
+            _DisplayServiceName(serviceName);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine($"| Waring | {eventDetails}");
+            Console.ResetColor();
 
         }
         public void Error(string serviceName, string eventDetails)
         {
-            System.Console.WriteLine($"Error | {serviceName} | {eventDetails}");
+            _DisplayServiceName(serviceName);
+            Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine($"| Error | {eventDetails}");
+            Console.ResetColor();
 
+        }
+        private void _DisplayServiceName(string serviceName)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.Write(serviceName);
+            Console.ResetColor();
         }
     }
 }
